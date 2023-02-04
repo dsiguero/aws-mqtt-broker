01586fc5ad9db682ad58eb7f87571498e476c719
dsiguero <26867647+dsiguero@users.noreply.github.com>
2023-02-04 18:09:12 +0100

    ci: follow sort from tf-docs config instead of command

--- ci/generate-tf-docs.sh
+++ ci/generate-tf-docs.sh
@@ -6,4 +6,4 @@ docker run -v "$(pwd)":/data \
     -e DELIM_START='<!-- BEGIN_TF_DOCS -->' \
     -e DELIM_CLOSE='<!-- END_TF_DOCS -->' \
     cytopia/terraform-docs:0.16.0 \
-    terraform-docs-replace-012 --sort-by required md README.md
+    terraform-docs-replace-012 md README.md
